acf473319b95dee658c6efdb5b059e7e9dfa8bbf
utpalbikash <32829484+utpalbikash@users.noreply.github.com>
2020-03-18 17:32:51 +0530

    MMsgBox("aaa")

--- WindowsApplication2/WindowsApplication2/Form1.vb
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
         MsgBox("aaa")
+        MsgBox("aaa")
     End Sub
 End Class
