34526c5d2dc60b98c528f0a943bf5473fb536f2d
utpalbikash <32829484+utpalbikash@users.noreply.github.com>
2020-03-18 13:31:51 +0530

    Add files via upload

---
+++ WindowsApplication2/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SuspendLayout()
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Location = New System.Drawing.Point(190, 119)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(141, 49)
+        Me.SimpleButton1.TabIndex = 0
+        Me.SimpleButton1.Text = "SimpleButton1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 261)
+        Me.Controls.Add(Me.SimpleButton1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+End Class
